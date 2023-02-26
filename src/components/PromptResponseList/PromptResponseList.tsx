fc204875ccbb66e3e717c81545548018be4388f5
Vinayak Singh <vinayaksingh10142@gmail.com>
2023-02-26 14:55:47 +0530

    code without the ellipses

--- src/components/PromptResponseList/PromptResponseList.tsx
+++ src/components/PromptResponseList/PromptResponseList.tsx
@@ -1,8 +1,8 @@
-import React, {FC, useEffect, useRef} from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import ChatGptImg from '../../img/chatgpt.png';
 import MyImg from '../../img/me.png';
 import ReactMarkdown from 'react-markdown';
-import {ResponseInterface} from "./response-interface";
+import { ResponseInterface } from './response-interface';
 import hljs from 'highlight.js';
 import './PromptResponseList.css';
 
@@ -15,7 +15,7 @@ const PromptResponseList: FC<PromptResponseListProps> = ({ responseList }) => {
 
   useEffect(() => {
     hljs.highlightAll();
-  })
+  });
 
   useEffect(() => {
     hljs.highlightAll();
@@ -24,26 +24,20 @@ const PromptResponseList: FC<PromptResponseListProps> = ({ responseList }) => {
   return (
     <div className="prompt-response-list" ref={responseListRef}>
       {responseList.map((responseData) => (
-        <div className={"response-container " + (responseData.selfFlag ? 'my-question' : 'chatgpt-response')} key={responseData.id}>
-          <img className="avatar-image" src={responseData.selfFlag ? MyImg : ChatGptImg} alt="avatar"/>
-          <div className={(responseData.error ? 'error-response ' : '') + "prompt-content"} id={responseData.id}>
-            { responseData.image &&
-                <img src={responseData.image} className="ai-image" alt="generated ai"/>
-            }
-            { responseData.response &&
+        <div className={'response-container ' + (responseData.selfFlag ? 'my-question' : 'chatgpt-response')} key={responseData.id}>
+          <img className="avatar-image" src={responseData.selfFlag ? MyImg : ChatGptImg} alt="avatar" />
+          <div className={(responseData.error ? 'error-response ' : '') + 'prompt-content'} id={responseData.id}>
+            {responseData.image && <img src={responseData.image} className="ai-image" alt="generated ai" />}
+            {responseData.response && (
               <ReactMarkdown
                 children={responseData.response ?? ''}
                 components={{
-                  code({className, children}) {
-                    return(
-                      <code className={className}>
-                        {children}
-                      </code>
-                    )
-                  }
+                  code({ className, children }) {
+                    return <code className={className}>{children}</code>;
+                  },
                 }}
               />
-            }
+            )}
           </div>
         </div>
       ))}
